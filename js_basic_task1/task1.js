d23d671b10b942db54836bc4b78fd708cf3a64ce
Oleh Solodkyi <truckman2895@gmail.com>
2024-02-26 19:27:29 +0200

    folder

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.bgGreen('Grass-test'));
+
+console.log(chalk.magentaBright('Panther'));
+
+console.log(chalk.red('Wine'));
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,10 @@
+let test = true
+let num = 60;
+let inf = num/0
+const name = "Brian";
+
+console.log(`His name is ${name}, he's ${num/2} years old (which is a ${typeof num}). He likes to say 'To ${inf} and Beyond'. That's ${test} statement.`)
+
+function getAge () {
+    console.log(num)
+}
