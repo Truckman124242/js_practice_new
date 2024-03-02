6c5c7e008a59eca0e854c743488b787858b67fb6
Oleh Solodkyi <truckman2895@gmail.com>
2024-03-02 17:35:15 +0200

    rewrote HW

---
+++ js_basic_task2_retry/task1.js
@@ -0,0 +1,22 @@
+let income = 8000
+let income_tax = income/100
+
+if (income < 10000) {
+    console.log(`The base income is $${income}. Tax will be $${(income_tax * 5)} while the cost with counted tax percent (5%) is $${income - income_tax * 5}`)
+}
+
+if (income >= 10000 && income <= 50000) {
+    console.log(`The base income is $${income}. Tax will be $${income_tax * 10} while the cost with counted tax percent (10%) is $${income - income_tax * 10}`)
+}
+
+ if (income >= 50001 && income <= 100000) {
+    console.log(`The base income is $${income}. Tax will be $${income_tax * 15} while the cost with counted tax percent (15%) is $${income - income_tax * 15}`)
+}
+
+ if (income >= 100001 && income <= 200000) {
+    console.log(`The base income is $${income}. Tax will be $${income_tax * 20} while the cost with counted tax percent (20%) is $${income - income_tax* 20}`) 
+}
+
+ if (income > 200000) {
+    console.log(`The base income is $${income}. Tax will be $${income_tax * 25} while the cost with counted tax percent (25%) is $${income - income_tax * 25}`)
+}
---
+++ js_basic_task2_retry/task2.js
@@ -0,0 +1,35 @@
+let base_price = 265700
+let car_year = 2015
+let car_run = 60000
+let has_climate_control = true
+let safe_pack = false
+let electric = false
+
+let result = base_price
+
+if (car_year >= 2019 && car_year <= 2022) {
+    result += base_price * 0.1
+    console.log (`Including the year ${car_year}, car price is ${result} now `)
+}
+
+if (car_run >= 50000 && car_run <= 100000) {
+    result -= base_price * 0.05
+    console.log (`Including the car having ${car_run} km, car price is ${result} now `)
+}
+
+if (has_climate_control) {
+    result += base_price * 0.07
+    console.log (`Including the climate control presence is ${has_climate_control}, car price is ${result} now `)
+}
+
+if (safe_pack) {
+    result += base_price * 0.08
+    console.log (`Including the safe pack presence is ${safe_pack}, car price is ${result} now `)
+}
+
+if (electric) {
+    result += base_price * 0.15
+    console.log (`Including the car being electric is ${electric}, car price is ${result} now `)
+}
+
+console.log(`Base car price is $${base_price} while the final price is $${result}. The change in price is $${result - base_price}.`)
